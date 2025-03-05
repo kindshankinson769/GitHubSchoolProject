dcc17143c81d26d94ea512e2601442be9493213f
kindshankinson769 <kindshankinson769@gmail.com>
2025-03-05 19:01:38 +0000

    Add 1.c

---
+++ src/1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int age = 21;
+    if(age > 18) {
+        printf("You are eligible to vote!\n");
+    } else {
+        printf("Sorry, you are not yet eligible to vote.\n");
+    }
+}
