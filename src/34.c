c65654080829705d5fcbfa22b8021270048f6c1c
kindshankinson769 <kindshankinson769@gmail.com>
2025-04-27 02:06:27 +0000

    Add 34.c

---
+++ src/34.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    int y = 10;
+
+    if (x > y) {
+        printf("X is greater than Y.\n");
+    } else {
+        printf("Y is greater than X.\n");
+    }
+
+    return 0;
+}
